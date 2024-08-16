e0e012cab7073dd97724564920e986f7fa4956a9
llh <linhai.li.cn@outlook.com>
2024-08-16 16:32:34 +0800

    什么情况？

--- src/main.rs
+++ src/main.rs
@@ -14,10 +14,13 @@ fn main() {
     // } else if number % 2 == 0 {
     //     println!("number is divisible by 2");
     // } else {
-    //     println!("number is not divisible by 4, 3, or 2");
+        // println!("number is not divisible by 4, 3, or 2");
     // }
 
     let condition = true;
     let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {number}");
+    println!("The value of number is: {number}");
+    println!("The value of number is: {number}");
     println!("The value of number is: {number}");
 }
